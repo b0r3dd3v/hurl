7ade30436c305c5c29c0b01c08da6c1a18a64bed
Todd <44746806+b0r3dd3v@users.noreply.github.com>
2021-03-01 13:15:23 +0300

    Create successapiv1.rs

---
+++ src/successapiv1.rs
@@ -0,0 +1,6 @@
+extern crate siphasher;
+
+use std::arch::x86_64;
+
+#[allow_deadcode]
+fn fullstack(woking_hoes: mut i64) { if(!woking_hoes){ panic!("Shimatta, iForgot 2 panik"); } else { fullstack(woking_hoes--); }
